89bc4cd461587f3811682f0a1a64410c41e38353
tackelua <tackelua@gmail.com>
2017-10-19 09:22:46 +0700

    add wifimulti

--- three-relays-blynk.ino
+++ three-relays-blynk.ino
@@ -1,168 +1,196 @@
-/*************************************************************
+﻿/*************************************************************
 Blynk project.
-Open this link and project will be auto creat on blynk-cloud
+Open this link and project will be auto created on blynk-cloud
 http://tinyurl.com/yd3zle46
  *************************************************************/
 
-/* Comment this out to disable prints and save space */
+ /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
 
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 #include <BlynkSimpleEsp8266.h>
+
+ESP8266WiFiMulti wifiMulti;
 
 #define DEBUG Serial
 #define DB(x) Serial.println(x)
-#define Db(x) Serial.print(x)
+#define Db(x) Serial.print(x) 
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char BLYNK_AUTH[] = "1e72d2ce724743ff98e3817d07a76edb";
+char BLYNK_AUTH[] = "13f528fc01334179874d55be89d04dc1";
 WidgetTerminal Terminal(V0);
 // Your WiFi credentials.
 // Set password to "" for open networks.
-//char ssid[] = "YourNetworkName";
-//char pass[] = "YourPassword";
+
+//char ssid[] = "Dingtea T2_sau";
+//char pass[] = "133nguyenvanlinh";
 
 #define R1 D2
-#define R2 D5
-#define R3 D6
+#define R2 D6
+#define R3 D5
 
 
-void setup()
-{
-  // Debug console
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(R1, OUTPUT);
-  pinMode(R2, OUTPUT);
-  pinMode(R3, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  wifi_init();
-  digitalWrite(LED_BUILTIN, HIGH);
+typedef enum { 
+	SMARTCONFIG = 1, 
+	WIFIMULTI = 2 
+} WiFi_Setup_Mode_t;
 
-  Blynk.config(BLYNK_AUTH, "blynk-cloud.com", 8442);
-  Serial.println(F("Connect to Blynk server"));
-  while (!Blynk.connect()) {
-    Serial.print(F("."));
-    delay(50);
-  }
-  
-  Blynk.syncAll();
-}
+void wifi_init(int mode) {
+	WiFi.setAutoConnect(true);
+	WiFi.setAutoReconnect(true);
+	WiFi.mode(WIFI_STA);
 
-void loop()
-{
-  Blynk.run();
-}
+	if (mode == SMARTCONFIG)
+	{
+		//Serial.println(F("SmartConfig started."));
+		//WiFi.beginSmartConfig();
+		//while (1) {
+		//  delay(1000);
+		//  if (WiFi.smartConfigDone()) {
+		//    Serial.println(F("SmartConfig: Success"));
+		//    WiFi.printDiag(Serial);
+		//    //WiFi.stopSmartConfig();
+		//    break;
+		//  }
+		//}
 
-void wifi_init() {
-  WiFi.setAutoConnect(true);
-  WiFi.setAutoReconnect(true);
-  WiFi.mode(WIFI_STA);
+		//WiFi.begin("Nokia 6", "mic12345678");
+		WiFi.printDiag(Serial);
+		Serial.println(F("\nConnecting..."));
+		if (WiFi.waitForConnectResult() == WL_CONNECTED)
+		{
+			Serial.println(F("connected\n"));
+		}
+		else
+		{
+			Serial.println(F("SmartConfig started."));
+			WiFi.beginSmartConfig();
+			while (1) {
+				delay(500);
+				if (WiFi.smartConfigDone()) {
+					Serial.println(F("SmartConfig: Success"));
+					WiFi.printDiag(Serial);
+					//WiFi.stopSmartConfig();
+					break;
+				}
+			}
+		}
+	}
 
-  //Serial.println(F("SmartConfig started."));
-  //WiFi.beginSmartConfig();
-  //while (1) {
-  //  delay(1000);
-  //  if (WiFi.smartConfigDone()) {
-  //    Serial.println(F("SmartConfig: Success"));
-  //    WiFi.printDiag(Serial);
-  //    //WiFi.stopSmartConfig();
-  //    break;
-  //  }
-  //}
-
-  WiFi.printDiag(Serial);
-
-  Serial.println(F("\nConnecting..."));
-
-  if (WiFi.waitForConnectResult() == WL_CONNECTED)
-  {
-    Serial.println(F("connected\n"));
-  }
-  else
-  {
-    Serial.println(F("connect again\n"));
-    if (WiFi.waitForConnectResult() == WL_CONNECTED)
-    {
-      Serial.println(F("connected\n"));
-      return;
-    }
-
-    Serial.println(F("SmartConfig started."));
-    WiFi.beginSmartConfig();
-    while (1) {
-      delay(500);
-      if (WiFi.smartConfigDone()) {
-        Serial.println(F("SmartConfig: Success"));
-        WiFi.printDiag(Serial);
-        //WiFi.stopSmartConfig();
-        break;
-      }
-    }
-  }
+	else if (mode = WIFIMULTI)
+	{
+		wifiMulti.addAP("Nokia 6", "mic12345678");
+		wifiMulti.addAP("GithAP", "giathinh123");
+		Serial.println("Connecting Wifi...");
+		if (wifiMulti.run() == WL_CONNECTED) {
+			Serial.println("");
+			Serial.println("WiFi connected");
+			Serial.println("IP address: ");
+			Serial.println(WiFi.localIP());
+		}
+	}
 }
 
 BLYNK_WRITE(V1) {
-  int buttonStt = param.asInt();
-  digitalWrite(R1, buttonStt);
-  Blynk.virtualWrite(V11, buttonStt);
-  Terminal.print("#V1: ");
-  Terminal.println(buttonStt);
-  Terminal.flush();
-  Db(F("#V1: "));
-  DB(buttonStt);
+	int buttonStt = param.asInt();
+	digitalWrite(R1, buttonStt);
+	Blynk.virtualWrite(V11, buttonStt);
+	Db(F("#V1: "));
+	DB(buttonStt);
+	Terminal.print("#V1: ");
+	Terminal.println(buttonStt);
+	Terminal.flush();
 }
 BLYNK_WRITE(V2) {
-  int buttonStt = param.asInt();
-  digitalWrite(R2, buttonStt);
-  Blynk.virtualWrite(V12, buttonStt);
-  Terminal.print("#V2: ");
-  Terminal.println(buttonStt);
-  Terminal.flush();
-  Db(F("#V2: "));
-  DB(buttonStt);
+	int buttonStt = param.asInt();
+	digitalWrite(R2, buttonStt);
+	Blynk.virtualWrite(V12, buttonStt);
+	Db(F("#V2: "));
+	DB(buttonStt);
+	Terminal.print("#V2: ");
+	Terminal.println(buttonStt);
+	Terminal.flush();
 }
 BLYNK_WRITE(V3) {
-  int buttonStt = param.asInt();
-  digitalWrite(R3, buttonStt);
-  Blynk.virtualWrite(V13, buttonStt);
-  Terminal.print("#V3: ");
-  Terminal.println(buttonStt);
-  Terminal.flush();
-  Db(F("#V3: "));
-  DB(buttonStt);
+	int buttonStt = param.asInt();
+	digitalWrite(R3, buttonStt);
+	Blynk.virtualWrite(V13, buttonStt);
+	Db(F("#V3: "));
+	DB(buttonStt);
+	Terminal.print("#V3: ");
+	Terminal.println(buttonStt);
+	Terminal.flush();
 }
 //====
 
 BLYNK_WRITE(V11) {
-  int buttonStt = param.asInt();
-  digitalWrite(R1, buttonStt);
-  Blynk.virtualWrite(V1, buttonStt);
-  Terminal.print("#V11: ");
-  Terminal.println(buttonStt);
-  Terminal.flush();
-  Db(F("#V11: "));
-  DB(buttonStt);
+	int buttonStt = param.asInt();
+	digitalWrite(R1, buttonStt);
+	Blynk.virtualWrite(V1, buttonStt);
+	Db(F("#V11: "));
+	DB(buttonStt);
+	Terminal.print("#V11: ");
+	Terminal.println(buttonStt);
+	Terminal.flush();
 }
 BLYNK_WRITE(V12) {
-  int buttonStt = param.asInt();
-  digitalWrite(R2, buttonStt);
-  Blynk.virtualWrite(V2, buttonStt);
-  Terminal.print("#V12: ");
-  Terminal.println(buttonStt);
-  Terminal.flush();
-  Db(F("#V12: "));
-  DB(buttonStt);
+	int buttonStt = param.asInt();
+	digitalWrite(R2, buttonStt);
+	Blynk.virtualWrite(V2, buttonStt);
+	Db(F("#V12: "));
+	DB(buttonStt);
+	Terminal.print("#V12: ");
+	Terminal.println(buttonStt);
+	Terminal.flush();
 }
 BLYNK_WRITE(V13) {
-  int buttonStt = param.asInt();
-  digitalWrite(R3, buttonStt);
-  Blynk.virtualWrite(V3, buttonStt);
-  Terminal.print("#V13: ");
-  Terminal.println(buttonStt);
-  Terminal.flush();
-  Db(F("#V13: "));
-  DB(buttonStt);
+	int buttonStt = param.asInt();
+	digitalWrite(R3, buttonStt);
+	Blynk.virtualWrite(V3, buttonStt);
+	Db(F("#V13: "));
+	DB(buttonStt);
+	Terminal.print("#V13: ");
+	Terminal.println(buttonStt);
+	Terminal.flush();
+}
+
+
+//=======================================
+
+void setup()
+{
+	// Debug console
+	Serial.begin(74880);
+	pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(R1, OUTPUT);
+	pinMode(R2, OUTPUT);
+	pinMode(R3, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW);
+
+	//WiFi.begin(ssid, pass);
+	wifi_init(WIFIMULTI);
+	digitalWrite(LED_BUILTIN, HIGH);
+
+	Blynk.config(BLYNK_AUTH, "blynk-cloud.com", 8442);
+	Serial.println(F("Connect to Blynk server"));
+	while ((wifiMulti.run() != WL_CONNECTED) && (!Blynk.connect())) {
+		Serial.print(F("."));
+		delay(50);
+	}
+
+	Blynk.syncAll();
+	Terminal.println(BLYNK_AUTH);
+}
+
+void loop()
+{
+	if (wifiMulti.run() != WL_CONNECTED) {
+		delay(1);
+		return;
+	}
+	Blynk.run();
+	delay(1);
 }
